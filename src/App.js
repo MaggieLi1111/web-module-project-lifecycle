43e757937a5cdb9539a622df4066c90aaead23cf
unknown <lidanyang1111@gmail.com>
2021-11-30 16:33:03 -0800

    version1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,73 @@
 import React from 'react';
+import User  from './components/User';
+import FollowerList from './components/FollowerList';
+import axios from "axios"
 
 class App extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      currentUser:"wlongmire",
+      user: {},
+      followers:[]
+    }
+  }
+
+  componentDidMount () {
+    axios.get(`https://api.github.com/users/${this.state.currentUser}`)
+    .then(res => {
+      console.log(res);
+      this.setState({
+        ...this.state,
+        user:res.data
+      });
+    })
+  }
+
+  componentDidUpdate (prevProps, prevState) {
+    if(this.state.user !== prevState.user) {
+      axios.get(`https://api.github.com/users/${this.state.currentUser}/followers`)
+    .then(res => {
+      this.setState({
+        ...this.state,
+        user:res.data
+      });
+    })
+    }
+  }
+
+
+  handleChange = (event) =>{
+    this.setState({
+      ...this.state,
+      currentUser:event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    axios.get(`https://api.github.com/users/${this.state.currentUser}`)
+    .then(res => {
+      this.setState({
+        ...this.state,
+        user:res.data
+      });
+    });
+  }
+
   render() {
-    return(<div>
-      Github Card
+    return(
+    <div>
+      <h1>Github Info</h1>
+      <form onSubmit={this.handleSubmit}>
+        <input placeholder="Github Handle" onChange={this.handleChange} />
+        <button>Search</button>
+      </form>
+
+      <User user={this.state.user}/>
+      <FollowerList followers={this.state.followers}/>
+
+
     </div>);
   }
 }
---
+++ src/components/Follower.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+class Follower extends React.Component {
+    render() {
+
+        const {follower} = this.props;
+
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+export default Follower;
